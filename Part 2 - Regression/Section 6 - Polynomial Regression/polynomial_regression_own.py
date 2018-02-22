11940283bfc939ec2afa2cfbf74f507e15cb2d06
brucewayne1248 <andreasserov@gmx.de>
2018-02-22 14:10:44 +0100

    regression finished

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression_own.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression_own.py
@@ -31,7 +31,36 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial regression to data set
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=2)
+poly_reg = PolynomialFeatures(degree=4)
+# X_poly has the x values of the polynomial model x, x^2, x^3 ...
 X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
-lin_reg2.fit(X_poly, y) 
+lin_reg2.fit(X_poly, y)
+
+# visualizing the linear regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff Linear Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# visualizing the polynomial regression
+#create X with higher resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+# transform higher resolution X_grid to matrix
+X_grid= X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff Polynomial Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+# just to see the y values of the regression
+y_poly = lin_reg2.predict(poly_reg.fit_transform(X))
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg2.predict(poly_reg.fit_transform(6.5))
